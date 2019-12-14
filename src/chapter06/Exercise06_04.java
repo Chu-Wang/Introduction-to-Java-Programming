41f4f58a4e4862ce3c22fed47809c35302697616
Chu-Wang <wang_mingming43@163.com>
2019-12-14 17:13:22 +0800

    一个问题，个位为0，反转数字

---
+++ src/chapter06/Exercise06_04.java
@@ -0,0 +1,26 @@
+package chapter06;
+
+/**
+ * @author WangMingMing
+ * @creat 2019-12-14 17:07
+ */
+public class Exercise06_04 {
+    public static void main(String[] args) {
+        System.out.print("Enter an integer: ");
+        java.util.Scanner input = new java.util.Scanner(System.in);
+        int number = input.nextInt();
+        reverse(number);
+    }
+
+    public static void reverse(int number) {
+        while (number != 0) {
+            int remainder = number % 10;
+            System.out.print(remainder);
+            number = number / 10;
+        }
+
+        System.out.println();
+    }
+}
+
+
